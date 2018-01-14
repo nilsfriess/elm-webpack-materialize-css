6e404fc2211238985701c67c7f97dd81d672eed0
Nils Friess <nils.friess@gmail.com>
2018-01-14 13:39:19 +0100

    starter project

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -10,46 +10,51 @@ import Components.Hello exposing ( hello )
 -- APP
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Html.program 
+      { init = (model, Cmd.none )
+      , view = view
+      , subscriptions = always Sub.none
+      , update = update
+      }
 
+-- TYPES
+
+type alias Task = 
+  { name : String }
 
 -- MODEL
-type alias Model = Int
+type alias Model = 
+  { tasks: List Task
+  }
 
 model : Model
-model = 0
+model =
+  { tasks = [] 
+  }
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg
+  = AddItem String
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    NoOp -> model
-    Increment -> model + 1
+    AddItem name ->
+      if String.length name /= 0 then
+        { model | tasks = model.tasks ++ [ { name = name } ] } ! []
+      else
+        model ! []
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+  nav []
+    [ div [ class "nav-wrapper" ]
+        [ a [ href "#", class "brand-logo" ] [ text "Day Planner" ]
+
         ]
-      ]
     ]
-  ]
 
 
 -- CSS STYLES
